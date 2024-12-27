2cf2bf2c9c357bbf694e3781d8ec30dec3ae4b77
Phongphorn2006 <pongporn2006@gmail.com>
2024-12-27 17:12:31 +0700

    compleat

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,44 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double myarrays[],int numarray,double aswer[]){
+   
+    double sum = 0, mean =0 ;
+    for(int i = 0;i < numarray;i++){
+        sum += myarrays[i];
+    }
+    mean = sum/numarray;
+    aswer[0] = mean;
+
+    double sum1 = 0,stdDevi = 0;
+    for(int i = 0;i < numarray; i++){
+        sum1 += pow(myarrays[i] - mean,2) ;
+    }
+    stdDevi =sqrt(sum1/numarray);
+    aswer[1] = stdDevi;
+
+    double Multi = 1, GeoMean = 0;
+    for(int i = 0;i < numarray; i++){
+        Multi = Multi * myarrays[i];  
+    }
+    GeoMean = pow(Multi,1.0/numarray);
+    aswer[2] = GeoMean;
+
+    double sum2 = 0,HarMean = 0;
+    for(int i = 0;i < numarray; i++){
+        sum2 += 1/myarrays[i];
+    }
+    HarMean = numarray/sum2;
+    aswer[3] = HarMean;
+
+    double max = myarrays[0] ,min = myarrays[0];
+    for(int i = 0;i < numarray; i++){
+        if(myarrays[i] > max) max = myarrays[i];
+        if(myarrays[i] < min) min = myarrays[i];
+    }
+    aswer[4] = max;
+    aswer[5] = min;
+}
+
+
